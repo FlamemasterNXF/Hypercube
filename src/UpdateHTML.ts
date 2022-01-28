a40fae43cbd205128d0cbcb702b4599064042bb8
FlamemasterNXF <flamemasternxf@gmail.com>
2022-01-28 16:00:55 -0500

    Cube UI

--- src/UpdateHTML.ts
+++ src/UpdateHTML.ts
@@ -8,4 +8,9 @@ export function UpdateHTML(){
     DOMCacheGetOrSet("mineralGen").innerText = `Your ${format(data.miners[0])} Miners mine ${format(temp.mineralsGen)} Rare Minerals/s, a Square requires 10 Rare Minerals`
 
     DOMCacheGetOrSet("compactButton").innerText = `Compact all of your Squares into ${format(temp.tempCubeGain[0])} Cubes \nCompact all of your Cubes into ${format(temp.tempCubeGain[1])} Tesseracts`
+
+    for (let i=0;i<data.cubes.length;i++){
+        let cubeNames:string[] = ["Cubes","Tesseracts","Penteracts","Hexeracts","Hepteracts","Octeracts","Enneracts","Decagons"]
+        DOMCacheGetOrSet(`cube${i}`).innerText = `You have ${format(data.cubes[i])} ${cubeNames[i]} \nEach produces ${format((i+1)*10)} Joules of energy`
+    }
 }
