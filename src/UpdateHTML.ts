e99ea1b22f73c2998e636f5fec5003f931a730d1
FlamemasterNXF <flamemasternxf@gmail.com>
2022-01-29 14:19:34 -0500

    New Compacting UI

--- src/UpdateHTML.ts
+++ src/UpdateHTML.ts
@@ -3,6 +3,8 @@ import { format, formatWhole, formatTime } from "./Formatting";
 import { D, globalData as data, globalTemp as temp } from "./Data";
 import Decimal from "break_eternity.js";
 
+export let cubeNames:string[] = ["Cubes","Tesseracts","Penteracts","Hexeracts","Hepteracts","Octeracts","Enneracts","Decagons"]
+
 export function UpdateHTML(){
     DOMCacheGetOrSet("squares").innerText = `You are in control of ${format(data.squares)} Squares. [+${format(temp.squaresGen)}/s]`
     DOMCacheGetOrSet("powerGen").innerText = `Your Hypercube generates ${format(temp.powerGen)} Joules of energy/s`
@@ -11,8 +13,10 @@ export function UpdateHTML(){
     DOMCacheGetOrSet("compactButton").innerText = `Compact all of your Squares into ${format(temp.tempCubeGain[0])} Cubes \nCompact all of your Cubes into ${format(temp.tempCubeGain[1])} Tesseracts`
 
     for (let i=0;i<data.cubes.length;i++){
-        let cubeNames:string[] = ["Cubes","Tesseracts","Penteracts","Hexeracts","Hepteracts","Octeracts","Enneracts","Decagons"]
         DOMCacheGetOrSet(`cube${i}`).innerText = `You have ${format(data.cubes[i])} ${cubeNames[i]} \nEach produces ${format((10**(i+1))*((i+1)))} Joules of energy`
+    }
+    for (let i=0;i<data.cubes.length;i++){
+        DOMCacheGetOrSet(`cubeCompact${i}`).innerText = `Compact your ${cubeNames[i]} into ${temp.tempCubeGain[i+1]} ${cubeNames[i+1]}`
     }
 
     DOMCacheGetOrSet("minerTexts").innerText = `You can currently sustain a maximum of ${format(temp.maxMiners)} Miners \nEach Miner requires 150J of energy`
