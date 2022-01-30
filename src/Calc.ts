d476a50685b197384f22fb62bed5dbbbef886206
FlamemasterNXF <flamemasternxf@gmail.com>
2022-01-30 13:37:19 -0500

    Implement Tech Effects

--- src/Calc.ts
+++ src/Calc.ts
@@ -5,22 +5,22 @@ export function calculateEnergyGain(){
     let contributions:Decimal[]= []
     let total = D(0)
     for(let i=0;i<data.cubes.length;i++){
-        contributions[i] = data.cubes[i].times((10**(i+1))*((i+1)))
+        contributions[i] = (data.cubes[i].times((10**(i+1))*((i+1)))).times(temp.techEffects[8])
         total = total.plus(contributions[i])
     }
     temp.powerGen = total
 }
 export function calculateMinerStuff(){
-    temp.mineralsGen = data.miners[0].times(10)
-    temp.maxMiners = temp.powerGen.div(150).floor()
+    temp.mineralsGen = ((data.miners[0].times(10)).times(temp.techEffects[1])).times(temp.techEffects[2])
+    temp.maxMiners = temp.powerGen.div(D(150).sub(temp.techEffects[3])).floor()
 }
 export function calculateSquareGain(){
     temp.squaresGen = temp.mineralsGen.div(10)
 }
 export function calculateCompact(){
-    temp.tempCubeGain[0] = data.squares.div(10)
+    temp.tempCubeGain[0] = data.squares.div(D(10).sub(temp.techEffects[7]))
     for (let i=1;i<data.cubes.length;i++){
-        temp.tempCubeGain[i] = data.cubes[i-1].div(10**(i+1))
+        temp.tempCubeGain[i] = (data.cubes[i-1].div(10**(i+1))).div(temp.techEffects[9])
     }
     for (let i=0;i<data.cubes.length;i++){
         if (temp.tempCubeGain[i].lt(1)) temp.tempCubeGain[i] = new Decimal(0)
@@ -41,7 +41,7 @@ export function calculateTechEffects(){
     temp.techEffects[7] = data.hasTech[7]?data.cubesUnlocked:D(0)
     temp.techEffects[8] = data.hasTech[8]?data.cubesUnlocked.div(10).plus(1):D(1)
     temp.techEffects[9] = data.hasTech[9]?D(1.1):D(1)
-    temp.techEffects[10] = data.hasTech[9]?D(1):D(0)
+    temp.techEffects[10] = data.hasTech[9]?D(1):D(0) //implementation WIP
     temp.techEffects[11] = D(1)
     temp.techEffects[12] = D(1)
 }
