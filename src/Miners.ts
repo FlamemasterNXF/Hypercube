67dd6e064fa860dda972f85aa61a90088fa9ba1f
FlamemasterNXF <flamemasternxf@gmail.com>
2022-04-20 20:15:27 -0400

    Clean out gh-pages branch

--- src/Miners.ts
+++
@@ -1,8 +0,0 @@
-import {globalData as data, globalTemp as temp, D} from "./Data";
-import Decimal from "break_eternity.js";
-export function buyMiner(){
-    if (data.miners[0].lte(temp.maxMiners) && data.cubes[0].gte(10)){
-        data.miners[0] = data.miners[0].plus(1)
-        data.cubes[0] = data.cubes[0].sub(10)
-    }
-}
